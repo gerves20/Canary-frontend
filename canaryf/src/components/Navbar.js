3e487094f714537763a6526613bc489169437daa
GabErves <Gabrielle.Erves@cix.csi.cuny.edu>
2022-07-01 18:34:10 -0400

    Week one-Login

---
+++ canaryf/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+import styled from "styled-components";
+import './Navbar.css';
+const Navbar = () =>{
+  return (
+  <div>
+    <li>
+      <Link  className={"link-styles"}  to="/">Home</Link>
+      <Link  className={"link-styles"}  to="/register">Register</Link>
+      <Link className={"link-styles"} to="/login">Login</Link>
+    </li>
+    
+    
+  </div>
+  );
+}
+export default Navbar;
---
+++ canaryf/src/components/pages/Login.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Navbar from '../Navbar';
+
+import { useState } from 'react';
+import './Login.css';
+
+const Login = () =>{
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The email you entered was: ${email}`)
+  }
+  
+  return (
+    <div>
+      {/* <Navbar/> */}
+      <h2 className={"link-styles2"}>Login</h2>
+      <form onSubmit={handleSubmit}>
+
+        <label 
+        type="text" 
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className={"email"}>Phone Number or Email
+        
+        <input type="text" />
+        </label>
+
+        <label 
+        type="text" 
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className={"password"}>Password
+        <input type="text" />
+
+        
+
+        </label>
+        <label >
+          
+        <input type="submit" 
+        className={"sumbit"}/>
+        </label>
+
+        
+      </form>
+
+     
+
+      
+    </div>
+    
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Login/>);
+
+
+export default Login;
