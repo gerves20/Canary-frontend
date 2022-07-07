36df27bdbf7ffc0409ebeaffb9123976f751b0ee
GabErves <Gabrielle.Erves@cix.csi.cuny.edu>
2022-07-07 17:14:52 -0400

    responsive login and register pages

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -5,13 +5,19 @@ import { TextInput } from 'react';
 import Select from 'react-select';
 
 import Navbar from '../Navbar';
+import FloatingLabel from "react-bootstrap-floating-label";
 
 import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
+
+  
+
 const Register = () =>{
+
+  
 
   
 
@@ -20,7 +26,18 @@ const Register = () =>{
   const [address, setAddress] = useState("");
   const [dob, setDob] = useState("");
   const [phonenum, setPhonenum] = useState("");
-  const [value, setValue] = React.useState('fruit');
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
+
+
+
+ 
+  const [value, setValue] = useState('');
   
 
   const handleSubmit = (event) => {
@@ -54,107 +71,28 @@ const Register = () =>{
     
     <div >
       
-      <h2>New Patient Registration</h2>
+      <h2 id="header">New Patient Registration</h2>
 
       
       
       
       <form onSubmit={handleSubmit} >
 
-      <div className="input-container">
+      <div className="input-container" >
       
-  
-
-
-</div>
-
-        <label 
-        type="text" 
-        name="fname"
-        value={fname}
-        onChange={(e) => setFname(e.target.value)}
-        className={"link-styles1"}>First Name* 
-        
-        <input type="text"/>
-        
-        </label>
-
-        {/* <h5 className={"link-styles1"}></h5> */}
-
-        <label 
-        type="text" 
-        name="lname"
-        value={lname}
-        onChange={(e) => setLname(e.target.value)}
-        className={"link-styles1"}>Last Name* 
-        <input type="text" />
-
-        
-
-        </label>
-        <label 
-        type="text" 
-        name="address"
-        placeholder="Your email here"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        className={"link-styles1"}>Address* 
-        <input type="text" />
-
-        
-
-        </label>
-        <label 
-        type="text" 
-        name="dob"
-        value={dob}
-        onChange={(e) => setDob(e.target.value)}
-        className={"link-styles1"}>Date Of Birth* 
-        <input type="text" />
-
-        
-
-        </label>
-        <label 
-        type="text" 
-        name="phonenum"
-        value={phonenum}
-        onChange={(e) => setPhonenum(e.target.value)}
-        className={"link-styles1"}>Phone Number* 
-        <input type="text" />
-
-        
-
-        </label>
-        <label
-        type="text" 
-        name="address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        className={"link-styles1"}>Country* 
-        <input type="text" />
-
-        
-
-        </label>
-        <label 
-        type="text" 
-        name="address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        className={"link-styles1"}>State* 
-        <input type="text" />
-
-        
-
-        </label>
-        
-
-
-        <div>
+      
+<FloatingLabel label=" First Name *" id="myLabel" style={{width: "70%"}}  value={fname} type="text" onChange1={(e) => setFname(e.target.value)} onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="Last Name* " id="myLabel" style={{width: "70%"}} value={lname} onChange1={(e) => setLname(e.target.value)} type="text" onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="Date Of Birth* " id="myLabel" style={{width: "70%"}} value={dob} type="text" onChange1={(e) => setDob(e.target.value)} onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="Address* " id="myLabel" style={{width: "70%"}} value={address} type="text" onChange1={(e) => setAddress(e.target.value)}onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="Country* " id="myLabel" style={{width: "70%"}}  value={country} type="text" onChange1={(e) => setCountry(e.target.value)} onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="State* " id="myLabel" style={{width: "70%"}} value={state} type="text" onChange1={(e) => setState(e.target.value)} onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="Zip Code* " id="myLabel" style={{width: "70%"}} value={zip} type="text" onChange1={(e) => setZip(e.target.value)} onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="Phone Number* " id="myLabel" style={{width: "70%"}} value={phonenum} type="text" onChange1={(e) => setPhonenum(e.target.value)} onChange={event => console.log(event.target.value) } />
+<div>
       <label className="col-md-8 col-offset-4" id= "dropdown">
         Gender*
-        <select value={value} onChange={handleChange}>
+        <select value={gender} onChange={handleChange} onChange1={(e) => setGender(e.target.value)}>
         {options.map((option) => (
             <option value={option.value}>{option.label}</option>
           ))}
@@ -162,68 +100,51 @@ const Register = () =>{
       </select>
       </label>
     </div>
+<FloatingLabel label="Email* " id="myLabel" style={{width: "70%"}} value={email} type="text" onChange1={(e) => setEmail(e.target.value)} name="email" onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="Username* " id="myLabel" style={{width: "70%"}} value={username} type="text" onChange1={(e) => setUsername(e.target.value)} onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="Password* " id="myLabel" style={{width: "70%"}} value={password} type="text" onChange1={(e) => setPassword(e.target.value)} onChange={event => console.log(event.target.value) } />
+<FloatingLabel label="Re-Type Password* " id="myLabel" style={{width: "70%"}} value={password} type="text" onChange1={(e) => setPassword(e.target.value)} onChange={event => console.log(event.target.value) } />
 
-    <label 
-        type="text" 
-        name="address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        className={"link-styles1"}>Email* 
-        <input type="text" />
+<label>
 
-        
+<button type="submit" className={"sumbit"} style={{width: "100%" }} class="btn btn-outline-dark btn-lg" id="button">Submit</button>
+</label>
 
-        </label>
+</div>
 
-        <label 
-        type="text" 
-        name="address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        className={"link-styles1"}>Username* 
-        <input type="text" />
+       
 
         
 
-        </label>
+        
+        
+        </form>
+        
+       
+        
+        
+
+
+    
+        
+        
+
+        
+
+        
+
+        
+        
+
+
+
+  
+ 
+ 
+        
+     
+        
       
-        <label 
-        type="text" 
-        name="address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        className={"link-styles1"}>Password* 
-        <input type="text" />
-
-        
-
-        </label>
-        <label 
-        htmlFor="name"
-        type="text" 
-        name="address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        className={"link-styles1"}
-        >Re-Type Password* 
-        <input type="text" />
-
-        
-
-        </label>
-
-        
-
-        
-
-        
-        <label>
-
-        <button type="submit" className={"sumbit"} class="btn btn-outline-dark btn-lg">Submit</button>
-        </label>
-
-        
-      </form>
 
 
 
